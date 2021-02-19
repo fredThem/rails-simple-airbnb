3fa5b20ba09cd110ea6ce8cf4bd574e6bca5ae2c
Frederic Desmarais <fdesmarais.them@gmail.com>
2021-02-19 15:11:14 -0500

    built models controllers routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :flats
+  # get 'flats/index'
+  # get 'flats/new'
+  # get 'flats/create'
+  # get 'flats/show'
+  # get 'flats/edit'
+  # get 'flats/update'
+  # get 'flats/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats"
+
+10.times do
+  flat = Flat.new(
+    name: Faker::Marketing.buzzwords,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.sentence(word_count: 12, supplemental: true),
+    price_per_night: Faker::Number.within(range: 75..300),
+    number_of_guests: Faker::Number.within(range: 2..10)
+  )
+  flat.save!
+  puts "#{flat.name}, $#{flat.price_per_night} per night"
+end
+puts "there are #{Flat.count} flats"
